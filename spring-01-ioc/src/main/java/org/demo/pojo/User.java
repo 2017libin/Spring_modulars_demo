997250e2b80a254561030093fd22d04e287a66ba
2017libin <1963482624@11.com>
2023-11-24 21:31:56 +0800

    init

--- spring-01-ioc/src/main/java/org/demo/pojo/User.java
+++ spring-01-ioc/src/main/java/org/demo/pojo/User.java
@@ -1,4 +1,21 @@
 package org.demo.pojo;
 
 public class User {
+    private String name;
+
+    public User() {
+        System.out.println("User的无参构造！");
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void show(){
+        System.out.println("name="+ name );
+    }
 }
