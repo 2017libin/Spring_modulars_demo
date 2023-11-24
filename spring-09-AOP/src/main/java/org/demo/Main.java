fbd0b15a18376a759fc9630fbb2a4f13b0f78429
2017chase <1963482624@qq.com>
2023-11-25 02:24:07 +0800

    add spring-09-AOP

---
+++ spring-09-AOP/src/main/java/org/demo/Main.java
@@ -0,0 +1,11 @@
+package org.demo;
+
+/**
+ * @Author ${USER}
+ * @Date ${DATE}
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ spring-09-AOP/src/main/java/org/demo/service/UserService.java
@@ -0,0 +1,12 @@
+package org.demo.service;
+
+/**
+ * @Author libin
+ * @Date 2023/11/25
+ */
+public interface UserService {
+    void add();
+    void delete();
+    void select();
+    void update();
+}
