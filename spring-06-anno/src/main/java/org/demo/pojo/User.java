0982b1fe0630dfc0a50b59d3f853cb68844676a3
2017chase <1963482624@qq.com>
2023-11-25 01:01:46 +0800

    update spring-06-anno

--- spring-06-anno/src/main/java/org/demo/pojo/User.java
+++ spring-06-anno/src/main/java/org/demo/pojo/User.java
@@ -1,10 +1,18 @@
 package org.demo.pojo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 // 等价于  <bean id="user" class="com.github.subei.pojo.User"/>
 // @Component 组件
 @Component
 public class User {
-    public String name = "哇哈哈";
+
+    // 相当于  <property name="name" value="subeiLY"/>
+    @Value("subeiLY")
+    public String name;
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
